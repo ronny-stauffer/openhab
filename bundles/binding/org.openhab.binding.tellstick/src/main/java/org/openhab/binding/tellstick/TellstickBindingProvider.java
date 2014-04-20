09c8597407b2cde44225ea9aa548a8a65e73565e
Ronny Stauffer <ronny.stauffer@wir-entwickeln.ch>
2014-04-20 13:11:18 +0200

    Added tellstick binding
    More error handeling
    
    Added javadoc and comments2
    
    Removed DS_Store
    
    Ignore .DS_Store
    
    Fixed javadoc and copyright

---
+++ bundles/binding/org.openhab.binding.tellstick/src/main/java/org/openhab/binding/tellstick/TellstickBindingProvider.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2013, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.tellstick;
+
+import java.util.EventListener;
+
+import org.openhab.binding.tellstick.internal.device.SupportedMethodsException;
+import org.openhab.binding.tellstick.internal.device.TellstickDevice;
+import org.openhab.core.binding.BindingProvider;
+
+/**
+ * This interface provides the method for getting the openhab configuration 
+ * for the tellstick binding.
+ * @author jbh
+ * @since 1.5.0
+ */
+public interface TellstickBindingProvider extends BindingProvider {
+	TellstickBindingConfig getTellstickBindingConfig(String itemName);
+	
+	TellstickBindingConfig getTellstickBindingConfig(int id, TellstickValueSelector valueSel);
+	
+	TellstickDevice getDevice(String itemName);
+	
+	void addListener(EventListener changeListener);
+	
+	void resetTellstickListener() throws SupportedMethodsException;
+
+	void removeTellstickListener();
+}
